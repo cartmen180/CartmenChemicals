86db0442fa70aa01c9dc4589fc68ecf396e4f5da
Hutch <cartmen180@gmail.com>
2014-10-20 18:29:52 +0200

    category change
    added png for item group

--- prototypes/category.lua
+++ prototypes/category.lua
@@ -1,8 +1,8 @@
 data:extend({
   { type = "item-group", name = "cartmen-base",
 	order = "cartmen-base",
-	inventory_order = "a-a",
-	icon = "__CartmenChemicals__/graphics/icons/salt.png",
+	inventory_order = "c-b",
+	icon = "__CartmenChemicals__/graphics/icons/group.png",
   },
   {type = "item-subgroup", name = "cartmen-basic-chemicals", group = "cartmen-base", order = "b-a"},
   {type = "item-subgroup", name = "cartmen-advanced-chemicals", group = "cartmen-base", order = "b-b"},
